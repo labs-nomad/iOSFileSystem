5454181ad39283f5ffcce78a52cb17ea18102f1d
Nomad Company <Nomad@Jons-MacBook-Pro-2.local>
2018-10-09 13:39:47 -0700

    bump 0.1.1

--- iOSFileSystem/Pods/Target Support Files/Pods-iOSFileSystem/Pods-iOSFileSystem-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSFileSystem : NSObject
-@end
-@implementation PodsDummy_Pods_iOSFileSystem
-@end
--- iOSFileSystem/Pods/Target Support Files/Pods-iOSFileSystemTests/Pods-iOSFileSystemTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iOSFileSystemTests : NSObject
-@end
-@implementation PodsDummy_Pods_iOSFileSystemTests
-@end
