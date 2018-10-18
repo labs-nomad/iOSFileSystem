8453c138082c314d8298dd107f79ae96ec80499a
Nomad Company <Nomad@Jons-MacBook-Pro-2.local>
2018-10-18 11:17:43 -0700

    Removed test file

--- iOSFileSystem/iOSFileSystemTests/DocumentsDirectory_Tests.swift
+++ iOSFileSystem/iOSFileSystemTests/DocumentsDirectory_Tests.swift
@@ -6,44 +6,44 @@
 //  Copyright © 2018 Nomad Company. All rights reserved.
 //
 
-import Quick
-import Nimble
-@testable import iOSFileSystem
-
-//The test class. This class will inherit from QuickSpec.
-class DocumentsDirectory_Tests: QuickSpec {
-    
-    /// Override the spec function of the `QuickSpec` superclass
-    override func spec() {
-        //Call super on the spec function
-        super.spec()
-        
-        //Any variables can go here
-        
-        
-        //This will be called once before any of the tests get run
-        beforeSuite {
-            
-        }
-        
-        //This will be called before each new test gets run
-        beforeEach {
-            
-        }
-        
-        //This is the test Template
-        describe("Description") {
-            context("Context", closure: {
-                it("Case", closure: {
-                    
-                })
-            })
-            context("Context", closure: {
-                it("Case", closure: {
-                    
-                })
-            })
-        }
-        
-    }
-}
+//import Quick
+//import Nimble
+//@testable import iOSFileSystem
+//
+////The test class. This class will inherit from QuickSpec.
+//class DocumentsDirectory_Tests: QuickSpec {
+//
+//    /// Override the spec function of the `QuickSpec` superclass
+//    override func spec() {
+//        //Call super on the spec function
+//        super.spec()
+//
+//        //Any variables can go here
+//
+//
+//        //This will be called once before any of the tests get run
+//        beforeSuite {
+//
+//        }
+//
+//        //This will be called before each new test gets run
+//        beforeEach {
+//
+//        }
+//
+//        //This is the test Template
+//        describe("Description") {
+//            context("Context", closure: {
+//                it("Case", closure: {
+//
+//                })
+//            })
+//            context("Context", closure: {
+//                it("Case", closure: {
+//
+//                })
+//            })
+//        }
+//
+//    }
+//}
