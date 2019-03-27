54a13551b0a50eb064f0d2680377effb15b9805f
Nomad Company <Nomad@Jons-MacBook-Pro-2.local>
2019-03-27 11:19:06 -0700

    bump 0.1.21

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@ podspec-bump -w
 git commit -am "bump `podspec-bump --dump-version`" 
 git tag "`podspec-bump --dump-version`"
 git push --tags
-pod trunk push iOSFileSystem.podspec --verbose
+pod trunk push iOSFileSystem.podspec --verbose --allow-warning
